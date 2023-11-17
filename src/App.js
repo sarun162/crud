2d0a20b57c5b3d38f814d87ae49dce2199f840be
sarunsaru <sarunsaru13@gmail.com>
2023-11-17 16:19:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser, deleteUser, updateUsername } from "./features/Users";
 
 function App() {
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [newUsername, setNewUsername] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div
+      className="App">
+        {" "}
+        <div className="addUser">
+          <input
+            type="text"
+            placeholder="Name..."
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Username..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <button style={{backgroundColor:'black',color:'white'}} 
+            onClick={() => {
+              dispatch(
+                addUser({
+                  id: userList[userList.length - 1].id + 1,
+                  name,
+                  username,
+                })
+              );
+            }}
+          >
+            {" "}
+            Add User
+          </button>
+        </div>
+  
+     
+      <div style={{color:'white',backgroundColor:'black',marginTop:'30px'}} className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div >
+              <h1> {user.name}</h1>
+              <h1> {user.username}</h1>
+              <input
+                type="text"
+                placeholder="New Username..."
+                onChange={(event) => {
+                  setNewUsername(event.target.value);
+                }}
+              />
+              <button
+                onClick={() => {
+                  dispatch(
+                    updateUsername({ id: user.id, username: newUsername })
+                  );
+                }}
+              >
+                {" "}
+                Update Username
+              </button>
+              <button
+                onClick={() => {
+                  dispatch(deleteUser({ id: user.id }));
+                }}
+              >
+                Delete User
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
